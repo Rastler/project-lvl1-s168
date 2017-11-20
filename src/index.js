9b76023eddbcf909fcb7762a364efaac200f660b
Roman Averin <rastler@gmail.com>
2017-11-20 17:10:47 +0300

    job02

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+const displayWelcome = () => {
+    console.log('Welcome to the Brain Games!');
+}
+
+const takeName = () => {
+    const name = readlineSync.question('May I have your name?: ');
+    console.log('Hello,', name);
+}
+
+export { displayWelcome, takeName };
+
