1a7654c784c2f4c1aac6df18ac50a99cbc4fe295
Roman Averin <rastler@gmail.com>
2017-11-24 22:34:11 +0300

    job07

---
+++ src/games/balance-game.js
@@ -0,0 +1,32 @@
+import { core, getRandomInt } from '..';
+
+const minRand = 10;
+const maxRand = 1000;
+const rules = 'Balance the given number.';
+
+const maxNumber = arr => Math.max(...arr);
+
+const minNumber = arr => Math.min(...arr);
+
+const balance = (arr) => {
+  const len = arr.length;
+  if (maxNumber(arr) - minNumber(arr) <= 1) {
+    return arr.join('');
+  }
+  const newArr = arr;
+  newArr.sort((a, b) => a - b);
+  newArr[0] += 1;
+  newArr[len - 1] -= 1;
+  return balance(newArr);
+};
+
+const game = () => {
+  const num = String(getRandomInt(minRand, maxRand));
+  const correct = balance([...num].map(el => Number(el)));
+  const conditions = `${num}`;
+  return { correct, conditions };
+};
+
+export default () => {
+  core(game, rules);
+};
