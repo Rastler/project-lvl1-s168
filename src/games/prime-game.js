cfb23793c3d8ad2a8eb82600b4129ce34c4a17ff
Roman Averin <rastler@gmail.com>
2017-11-25 23:51:09 +0300

    job09

---
+++ src/games/prime-game.js
@@ -0,0 +1,30 @@
+import { core, getRandomInt } from '..';
+
+const maxNumber = 30;
+const rules = 'Guess a prime number or not.';
+
+const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
+  const iter = (n) => {
+    if ((number % n) === 0) {
+      return false;
+    }
+    if ((n * n) <= number) {
+      return iter(n + 1);
+    }
+    return true;
+  };
+  return iter(2);
+};
+
+const gameGenerator = () => {
+  const number = getRandomInt(1, maxNumber);
+  const question = `${number}`;
+  return { correct: isPrime(number) ? 'yes' : 'no', question };
+};
+
+export default () => {
+  core(gameGenerator, rules);
+};
