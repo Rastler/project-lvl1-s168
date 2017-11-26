1277781dccc6d2e0fad8f156204ae417245c5a12
Roman Averin <rastler@gmail.com>
2017-11-26 14:02:00 +0300

    fix comment

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,34 +1,33 @@
 import { core, getRandomInt } from '..';
 
-const size = 10;
+const progSize = 10;
 const maxStepRand = 5;
 const rules = 'What number is missing in this progression?';
 
-const progressionGenerator = (start, step, lost) => {
-  let lostNumber;
-  const sequence = [];
+const progressionGenerator = (start, step, size) => {
+  const progArr = [];
   const iter = (index) => {
     if (index > size - 1) {
-      return { lostNumber, sequence };
+      return progArr;
     }
-    if (lost === index) {
-      lostNumber = start + (step * lost);
-      return iter(sequence.push('..'));
-    }
-    return iter(sequence.push(start + (step * index)));
+    return iter(progArr.push(start + (step * index)));
   };
   return iter(0);
 };
 
 const gameGenerator = () => {
-  const lost = getRandomInt(0, size - 1);
+  const lostPosition = getRandomInt(0, progSize - 1);
   const start = getRandomInt(1, 5);
   const step = getRandomInt(1, maxStepRand);
-  const progression = progressionGenerator(start, step, lost);
-  const correct = String(progression.lostNumber);
-  const sequence = progression.sequence.join(' ');
-  const question = `${sequence}`;
-  return { correct, question };
+  const progression = progressionGenerator(start, step, progSize);
+  const correct = progression[lostPosition];
+  const sequence = progression.map((el, index) => {
+    if (index === 2) {
+      return '..';
+    }
+    return el;
+  });
+  return { correct, question: `${sequence.join(' ')}` };
 };
 
 export default () => {
